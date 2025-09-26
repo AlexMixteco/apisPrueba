13d274c8363f4da7e2e81aff4e2314c92556c1a0
AlejandroMixteco <alejandro.mixteco.24e@utzmg.edu.mx>
2025-09-26 13:29:36 -0600

    apis

--- connection.js
+++ connection.js
@@ -2,15 +2,16 @@ const { text } = require('express');
 const {Pool} = require ('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    password: '123',
-    host: 'localhost',
-    port: 5432,
-    database: 'grupo_EB'
-    //ssl:true
-
+  user: process.env.USER,
+  password: process.env.PASS,
+  host: process.env.HOST,
+  port: 5432,
+  database: process.env.DATABASE,
+  ssl: {
+    require: true,
+  }
 });
 
 module.exports ={
     query:(text,params) => pool.query(text,params)
-};
+};  
