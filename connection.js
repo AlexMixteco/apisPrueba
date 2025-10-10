3ef9edba9138f6f914e7d3eb5b4e751d224136eb
AlejandroMixteco <alejandro.mixteco.24e@utzmg.edu.mx>
2025-10-10 13:33:51 -0600

    vamos

--- connection.js
+++ connection.js
@@ -1,18 +1,17 @@
-const { text } = require('express');
-const {Pool} = require ('pg');
-require('dotenv').config(); 
+const { Pool } = require('pg');
+require('dotenv').config(); // Cargar variables del .env
 
 const pool = new Pool({
-  user: process.env.USER,
-  password: process.env.PASS,
-  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   port: 5432,
-  database: process.env.DATABASE,
- ssl: {
-  require: true,
- }
+  ssl: {
+    rejectUnauthorized: false // necesario para Render
+  }
 });
 
-module.exports ={
-    query:(text,params) => pool.query(text,params)
-};  
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
